cbd54f6487d3224cf0862d98de74e97efdcb329a
Rafael Porreca <rafael.porreca@gmail.com>
2018-05-22 14:24:09 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
 import { AlertController } from "ionic-angular";
+import { FieldMessage } from "../models/fieldmessage";
 
 @Injectable()
 export class ErrorIntecerptor implements HttpInterceptor {
@@ -21,9 +22,6 @@ export class ErrorIntecerptor implements HttpInterceptor {
             if(!errorObj.status){
                 errorObj = JSON.parse(errorObj);
             }
-            
-            console.log("Erro detectado pelo interceptor");
-            console.log(errorObj);
 
             switch(errorObj.status){
                 case 401:
@@ -31,6 +29,10 @@ export class ErrorIntecerptor implements HttpInterceptor {
                     break;
                 case 403: 
                     this.handle403();
+                    break;
+
+                case 422:
+                    this.handle422(errorObj);
                     break;
 
                 default:
@@ -58,6 +60,20 @@ export class ErrorIntecerptor implements HttpInterceptor {
     handle403(){
         this.storage.setLocalUser(null);
     }
+
+    handle422(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Erro de validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons:[
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();
+    }
     
     handleDefaultError(errorObj){
         let alert = this.alertCtrl.create({
@@ -71,6 +87,14 @@ export class ErrorIntecerptor implements HttpInterceptor {
             ]
         });
         alert.present();
+    }
+
+    private listErrors(messages: FieldMessage[]) : string{
+        let s: string = '';
+        for(var i=0; i<messages.length;i++){
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
     }
 }
 
