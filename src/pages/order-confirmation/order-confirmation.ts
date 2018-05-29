0d76b290bae891d175f6a1c9d2280b0a9fbce040
Rafael Porreca <rafael.porreca@gmail.com>
2018-05-29 14:45:39 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 /**
  * Generated class for the OrderConfirmationPage page.
@@ -26,8 +27,15 @@ export class OrderConfirmationPage {
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public cartService: CartService, public clienteService: ClienteService) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public cartService: CartService,
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
+
     this.pedido = this.navParams.get('pedido');
+
   }
 
   ionViewDidLoad() {
@@ -45,6 +53,24 @@ export class OrderConfirmationPage {
   total(){
     return this.cartService.total();
   }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  checkout(){
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {        
+        console.log(response.headers.get('location'));
+        this.cartService.createOrClearCart();
+      },
+    error => {
+      if(error.status == 403){
+        this.navCtrl.setRoot('HomePage');
+      }
+    });
+  }
+
   private findEndereco(id: string, list: EnderecoDTO[]) : EnderecoDTO{
     let position = list.findIndex(x => x.id == id);
     return list[position];
