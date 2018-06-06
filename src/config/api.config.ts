7687495d689f4045ec22c0e05afcb38ed0c7bf49
Rafael Porreca <rafael.porreca@gmail.com>
2018-06-06 10:27:16 -0300

    testando app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl : "http://localhost",
+    baseUrl : "https://loja-virtual-curso.herokuapp.com",
     bucketBaseUrl : "https://s3-sa-east-1.amazonaws.com/lojavirtual-curso"
 }
